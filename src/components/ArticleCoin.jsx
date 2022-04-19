ff150b51e4e4632ff2ea2df0bd3857a7fed6fcd7
Matias Agelvis <Magelvisdoz@gmail.com>
2022-04-19 11:53:38 -0400

    Add one day price trend to each article card
    Create a 30/70 split in the cards to fit the price chart

--- src/components/ArticleCoin.jsx
+++ src/components/ArticleCoin.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Card, Button, Badge } from "react-bootstrap";
 import { BsStar } from "react-icons/bs";
 import '../styles/ArticleCoin.css';
+import Trend from 'react-trend';
 
 const ArticleCoin = (props) => {
   const dollarUSLocale = Intl.NumberFormat("en-US");
   const { id, symbol, name, priceUsd, explorer } = props;
+  const [prices, setPrices] = useState([])
+
+  useEffect(() => {
+    async function getData () {
+      let promise = await fetch(`https://api.coincap.io/v2/assets/${id}/history?interval=d1`)
+      let results = await promise
+      let rawData = await results.json()
+      console.log(id, props, rawData.data.map(x => x.priceUsd))
+      setPrices(rawData.data.map(x => parseFloat(x.priceUsd)))
+    }
+    getData()
+  }, [name]);
 
   return (
     <>
@@ -20,16 +33,24 @@ const ArticleCoin = (props) => {
                 <BsStar  className="Icon__favorite" onClick={() => console.log('ADD TO FAVORITE',id)} />
               </div>
             </Card.Title>
-            <Card.Subtitle>
+            <div className='vsplit-body'>
+            <div className='vsplit-left'>
+
               Price Today : $ {dollarUSLocale.format(priceUsd)}
               <br />
               <br />
               {explorer}
-            </Card.Subtitle>
+
+            <br />
             <br />
             <Button variant="outline-info" href={explorer} target="_blank">
               Go to page
             </Button>
+            </div>
+            <div className='vsplit-right'>
+             <Trend data={prices} gradient={['#284CB2', '#35DAF7', '#F96CA8']}/> 
+            </div>
+            </div>
           </Card.Body>
         </Card>
       </Container>
